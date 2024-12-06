01c1f82fe4cf2433956ca5c9366c17cb4ee7991a
Lucas Moreira Albino <159702740+snortexware@users.noreply.github.com>
2024-12-05 21:42:21 -0300

    Adicionar arquivos de projeto.

---
+++ envioBoletos/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace envioBoletos
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Service service = new Service();
+            service.ExtractData();
+            service.ShowData();
+        }
+    }
+}
